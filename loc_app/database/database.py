66fb3640a3ff4d0577089df2e1590aab0b84c7e5
Ahmed Pyar Ali <aali@foreepay.com>
2018-06-04 00:05:17 +0500

    read db

--- loc_app/database/database.py
+++ loc_app/database/database.py
@@ -7,6 +7,7 @@ from loc_app.helpers.utils import log
 
 @coroutine
 def database_read_one(collection, read_filter={}, result_filter={}):
+    results = None
     if db:
         db_collection = db[collection]
         if not db_collection:
@@ -14,3 +15,20 @@ def database_read_one(collection, read_filter={}, result_filter={}):
 
         results = yield db_collection.find_one(read_filter, result_filter)
         log(results)
+
+    return results
+
+
+@coroutine
+def database_read_many(collection, read_filter={}, result_filter={}):
+    results = None
+    if db:
+        db_collection = db[collection]
+        if not db_collection:
+            db_collection = yield db.create_collection(collection)
+
+        results = yield db_collection.find(read_filter, result_filter)
+        log(results)
+
+    return results
+    
