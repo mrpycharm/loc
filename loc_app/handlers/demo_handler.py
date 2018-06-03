d933decd7014c3c25738b01bce20d27623455e61
Ahmed Pyar Ali <aali@foreepay.com>
2018-06-03 17:13:49 +0500

    test sleep and croutine

--- loc_app/handlers/demo_handler.py
+++ loc_app/handlers/demo_handler.py
@@ -5,5 +5,7 @@ from tornado.gen import coroutine, sleep
 class MainHandler(RequestHandler):
 	@coroutine
 	def get(self):
+		print('Request came in\n')
 		yield sleep(5)
+		print('writing reponse\n')
 		self.write('Hello World!\n')
