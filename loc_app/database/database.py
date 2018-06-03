62886088d460eaaa9141eaead7c6bff3d900c4ba
Ahmed Pyar Ali <aali@foreepay.com>
2018-06-04 00:52:36 +0500

    read db

--- loc_app/database/database.py
+++ loc_app/database/database.py
@@ -54,3 +54,33 @@ def database_insert_many(collection, data, ordered=True):
         if isinstance(data, list):
             results = yield db_collection.insert_many(data, ordered=ordered)
             log(results)
+
+
+@coroutine
+def database_update(collection, read_filter, update, multi=False):
+    results = None
+    if db:
+        db_collection = db[collection]
+        if not db_collection:
+            db_collection = yield db.create_collection(collection)
+
+        if not multi:
+            results = yield db_collection.update_one(read_filter, update)
+        else:
+            results = yield db_collection.update_many(read_filter, update)
+        log(results)
+
+
+@coroutine
+def database_delete(collection, read_filter, multi=False):
+    results = None
+    if db:
+        db_collection = db[collection]
+        if not db_collection:
+            db_collection = yield db.create_collection(collection)
+
+        if not multi:
+            results = yield db_collection.delete_one(read_filter)
+        else:
+            results = yield db_collection.delete_many(read_filter)
+        log(results)
--- loc_app/handlers/demo_handler.py
+++ loc_app/handlers/demo_handler.py
@@ -2,15 +2,13 @@ from tornado.web import RequestHandler
 from tornado.gen import coroutine
 
 # --- app module imports
-from loc_app.database.database import database_insert_many
+from loc_app.database.database import database_insert_one, database_update, database_delete
 
 
 class MainHandler(RequestHandler):
 	@coroutine
 	def get(self):
-		data = [
-			{'name' : 'hadi'},
-			{'name' : 'rana'}
-		]
-		yield database_insert_many('user', data)
+		yield database_insert_one('user', {'name' : 'Ahmed'})
+		yield database_update('user', {'name' : 'Ahmed'}, {'$set' : {'name' : 'Abdullah'}})
+		yield database_delete('user', {'name' : 'Ahmed'})
 		self.write('Hello World!\n')
