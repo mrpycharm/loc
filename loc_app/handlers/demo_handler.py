16507f8d2aa9e40e1ecaba4149d4b33234c3459c
Ahmed Pyar Ali <aali@foreepay.com>
2018-06-04 00:55:32 +0500

    db integration complete with motor

--- loc_app/handlers/demo_handler.py
+++ loc_app/handlers/demo_handler.py
@@ -2,11 +2,9 @@ from tornado.web import RequestHandler
 from tornado.gen import coroutine
 
 # --- app module imports
-from loc_app.database.database import database_insert_one, database_update, database_delete
 
 
 class MainHandler(RequestHandler):
 	@coroutine
 	def get(self):
-		yield database_delete('user', {'name' : 'Abdullah'})
 		self.write('Hello World!\n')
